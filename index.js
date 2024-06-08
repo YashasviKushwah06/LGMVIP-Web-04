1c511a89c5f2a91253cafa3ad445d025562d55c8
Yashasvi Kushwah <135824083+YashasviKushwah06@users.noreply.github.com>
2024-06-08 22:08:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let string = "";
+let buttons = document.querySelectorAll(".button");
+Array.from(buttons).forEach((item) => {
+    item.addEventListener('click', (e) => {
+        if(e.target.innerHTML == "="){
+            string = eval(string);
+            document.querySelector("input").value = string;
+        }
+        else if(e.target.innerHTML == "AC"){
+            string = "";
+            document.querySelector("input").value = string;
+        }
+        else if(e.target.innerHTML == "DE"){
+            string = string.slice(0,-1);
+            document.querySelector("input").value = string;
+        }
+        else{console.log(e.target)
+        string = string + e.target.innerHTML;
+        document.querySelector("input").value = string;}
+    })
+})
